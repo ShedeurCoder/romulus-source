e08312f7dd0a3654ab22893c75d0c44681900dbd
Shedeur <81215635+ShedeurCoder@users.noreply.github.com>
2023-07-05 13:56:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import discord
 import random
 import time
 #from keep_alive import keep_alive
-#TO ADD HANGMAN AND GUESS COUNTRY
-client = discord.Client()
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
 flags = ["Afghanistan", "Albania","Algeria","Andorra","Angola","Antigua and Barbuda", "Argentina","Armenia","Australia","Austria","Azerbaijan","The Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bhutan","Bolivia","Bosnia and Herzegovina","Botswana","Brazil","Brunei","Bulgaria","Burkina Faso","Burundi","Cabo Verde", "Cambodia","Cameroon","Canada","Central African Republic","Chad","Chile","China","Colombia","Comoros","Congo","Costa Rica", "Cote D'Ivoire", "Croatia", "Cuba","Cyprus","Czechia","North Korea","Democratic Republic of the Congo","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Eswatini","Ethiopia","Fiji","Finland","France","Gabon","The Gambia", "Georgia","Germany","Ghana","Greece","Grenada","Guatemala","Guinea","Guinea-Bissau","Guyana","Haiti","Honduras","Hungary","Iceland","India","Indonesia",'Iran','Iraq','Ireland','Israel','Italy','Jamaica','Japan',"Jordan",'Kazakhstan','Kenya','Kiribati','Kuwait','Kyrgyzstan','Laos','Latvia','Lebanon','Lesotho','Liberia','Libya','Liechtenstein','Lithuania','Luxembourg','Madagascar','Malawi','Malaysia','Maldives','Mali','Malta','Marshall Islands','Mauritania','Mauritania','Mauritius','Mexico','Micronesia','Monaco','Mongolia','Montenegro','Morocco','Mozambique','Myanmar','Namibia','Nauru','Nepal','Netherlands','New Zealand','Nicaragua','Niger','Nigeria','North Macedonia','Norway','Oman','Pakistan','Palau','Palestine','Panama',"Papua New Guinea",'Paraguay','Peru','Philippines','Poland','Portugal','Qatar','South Korea','Moldova','Romania','Russia','Rwanda',"Saint Kitts and Nevis",'Saint Lucia','Saint Vincent and the Grenadines','Samoa','San Marino','Sao Tome and Principe','Saudi Arabia','Senegal','Serbia','Seychelles','Sierra Leone','Singapore','Slovakia','Slovenia','Solomon Islands','Somalia','South Africa','South Sudan','Spain','Sri Lanka','Sudan','Suriname','Sweden','Switzerland','Syria','Tajikistan','Thailand','Timor-Leste','Togo','Tonga','Trinidad and Tobago','Tunisia','Turkey','Turkmenistan','Tuvalu','Uganda','Ukraine','United Arab Emirates','United Kingdom','Tanzania','United States of America','Uruguay','Uzbekistan','Vanuatu','Venezuela','Vatican City','Vietnam','Yemen','Zambia','Zimbabwe']
 
-words = ["Ababsbasbibasadadidasdidas","Romulus","CharlieBrown","Dulluh","JoeBiden","IbioYuh","Juancho","Chibib", "Chiboo", "QuantumPhysics", "AMOGUS", "MINECRAFT","SteveJobless", "Remus", 'Derpy', 'Chicken','Rat','Television','iPhone13']
+gwen = ['1.png', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.png', '9.jpg', '10.jpg', '11.jpg', '12.png', '13.jpg', '14.png', '15.png']
+
+words = ["Ababsbasbibasadadidasdidas","Romulus","CharlieBrown","Dulluh","Chibib","Chiboo","SteveJobless", 'hampter', 'Chicken','Gort','TimCookware', 'Chibroody', 'BigOunce', 'QuandaleDingle', 'Dababy', 'ElChapo', 'Romulus', 'Donkah', 'MangInasal', 'BurgerKing', 'TheChatterbox', 'Chachachombor', 'CarMechanic', 'PatrickBateman', 'Javascript', 'WalterWhite', 'VladimirGwenin', 'JorgenWurtz', 'GwenStacy', 'Bnuuy', 'CharlesDeMarcs', 'NeverGonnaGiveYouUp', 'ChooksToGo', 'KingPin', 'PhantasticGus', 'RahanTrahan', 'Spock', 'LinusTechTips', 'TuriIpIpIp', 'NoCopyrightSounds', 'VisualStudioCode', 'HalfAsInteresting']
 
 hangman = False
 countryguesson = False
@@ -69,7 +73,11 @@ async def on_message(message):
             await s.edit(content=seconds)
         await s.edit(content="TIMER FINISHED")
     if message.content.startswith(".help"):
-        await message.channel.send("**COMMAND LIST**\n\n.help - *Shows this message*\n.chachacha - *Chachacha your way into heaven* \n.fish - *fish go blub blub*\n.communism - *SHOWS EVERYONE'S FAVORITE IDEOLOGY*\n.say - *Says anything you want it to say* \n.edandem - *Best characters ever* \n.dice (dice number) - *Rolls a number of dice specified* \n.steve - *Yes or No answered by STEVE*\n.timer (seconds) - *Timer for seconds specified*\n.hangman - *Do da hangman*\n.country - *Guess da countryflag*\n**THATS ALL THE COMMANDS FOR ROMULUS**")
+        await message.channel.send("# COMMAND LIST\n\n**.help** - *Shows this message*\n**.chachacha** - *Chachacha your way into heaven* \n**.fish** - *fish go blub blub*\n**.communism** - *SHOWS NO ONE'S FAVORITE IDEOLOGY*\n**.say** - *Says anything you want it to say* \n**.edandem** - *Best characters ever* \n**.gwen** - *my love* \n**.dice** (dice number) - *Rolls a number of dice specified* \n**.steve** - *Yes or No answered by STEVE*\n**.timer** (seconds) - *Timer for seconds specified*\n**.hangman** - *Do da hangman*\n**.country** - *Guess da countryflag*\n**THATS ALL THE COMMANDS FOR ROMULUS**")
+
+    if message.content.lower().startswith('.gwen'):
+        gwenPic = gwen[random.randint(0, len(gwen) - 1)]
+        await message.channel.send(file=discord.File("gwen/" + gwenPic))
 
     #MAKE THE HANGMAN
     if message.content.startswith(".hangman"):
@@ -77,9 +85,11 @@ async def on_message(message):
         userhangman = message.author.id
         global hangmantries
         hangmantries = 15
+        global hangmanLetters
+        hangmanLetters = []
         #pick a word
         global word
-        word = words[random.randint(0,len(words))]
+        word = words[random.randint(0,len(words) - 1)]
         global tempword
         tempword = ""
         for char in word:
@@ -90,27 +100,35 @@ async def on_message(message):
 
     if hangman == True and len(message.content) == 1:
         if userhangman == message.author.id:
-                if hangmantries > 0:
-                    global charnumber
-                    charnumber = 0
+            if hangmantries > 0:
+                hangmanLetters.append(message.content.lower())
+                letters = ''
+                for letter in hangmanLetters:
+                    if len(letters) == 0: 
+                        letters = '**' + letter + '**'
+                        continue
+                    letters = letters + ', **' + letter + '**'
+                global charnumber
+                charnumber = 0
 
-                    prevtemp = tempword
 
-                    for char in word:
-                        if message.content.lower() == char.lower():
-                            tempword = tempword[:charnumber] + word[charnumber] + tempword[charnumber + 1:]
-                        charnumber = charnumber + 1
-                    if tempword == prevtemp:
-                        hangmantries = hangmantries - 1
-                    
-                    await message.channel.send(tempword + "\nYou have " + str(hangmantries).lower() + " tries left.")
+                prevtemp = tempword
 
-                    if tempword == word:
-                        await message.channel.send("Congratulations! You solved it")
-                        hangman = False
-                else:
-                    await message.channel.send("No tries left! You lost.")
+                for char in word:
+                    if message.content.lower() == char.lower():
+                        tempword = tempword[:charnumber] + word[charnumber] + tempword[charnumber + 1:]
+                    charnumber = charnumber + 1
+                if tempword == prevtemp:
+                    hangmantries = hangmantries - 1
+                
+                await message.channel.send(tempword + "\nYou have " + str(hangmantries).lower() + " tries left.\nAttempted letters: " + letters)
+
+                if tempword == word:
+                    await message.channel.send("Congratulations! You solved it")
                     hangman = False
+            else:
+                await message.channel.send("No tries left! You lost.")
+                hangman = False
         else:
             return
 
@@ -119,12 +137,12 @@ async def on_message(message):
         global userflags
         userflags = message.author.id
         global country
-        country = flags[random.randint(0, len(flags))]
+        country = flags[random.randint(0, len(flags) - 1)]
         global flagsinarow
         flagsinarow = 0
         global countryfile
         countryfile = country.replace(" ", "")
-        await message.channel.send(file=discord.File(countryfile + ".png"))
+        await message.channel.send(file=discord.File("flags/" + countryfile + ".png"))
         global countryguesson
         countryguesson = True
         return
@@ -134,7 +152,7 @@ async def on_message(message):
                 flagsinarow = flagsinarow + 1
                 country = flags[random.randint(0, len(flags))]
                 countryfile = country.replace(" ", "")
-                await message.channel.send(file=discord.File(countryfile + ".png"))
+                await message.channel.send(file=discord.File("flags/" + countryfile + ".png"))
             else:
                 await message.channel.send("Wrong, it was **" + country + "**. You got " + str(flagsinarow) + " flags in a row.")
                 countryguesson = False 
@@ -142,4 +160,4 @@ async def on_message(message):
             return
 
 #keep_alive()
-client.run('***')
+client.run('OTUyNTE0MTMzOTEwMjI5MDQz.Yi3H6g.u71wDriEVOWEDgg1z_y4jsIvrqk')
